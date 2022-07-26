060f29820198d627b30ac9a77bf903cedd8f3230
Praveen Kumar  K S <praveenkumarks11@gmail.com>
2022-07-25 20:56:46 -0700

    project setup

---
+++ ecom-client/src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+    sku : string | undefined;
+    name : string | undefined;
+    description : string | undefined;
+    unitPrice : number | undefined;
+    imageUrl : string | undefined;;
+    active : boolean | undefined;;
+    unitInStocks : number | undefined;;
+    dateCreated : Date | undefined;;
+    lastUpdated : Date | undefined;;
+
+}
